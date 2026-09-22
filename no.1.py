859ad052bfaf746e8a8f4470378fae9ff8245de3
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-09-22 15:52:39 +0700

    no 1

--- no.1.py
+++ no.1.py
@@ -12,18 +12,3 @@ else:
     print("Not my concern, sorry")
 
 
-#2
-print("A. 10, B. 99, C. 99,5")
-number="A","B"
-while number != "C":
-    number = input("Choose the big one! : ")
-print("Nice, good choice!")
-
-
-#3S
-for steps in range(10):
-    print(S)
-
-#4
-
-#5
