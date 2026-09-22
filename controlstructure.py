53d20019cfe1199c1f48a283024c0883710836bb
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-09-22 15:48:34 +0700

    first commit

---
+++ controlstructure.py
@@ -0,0 +1,21 @@
+#1
+nilai = input("Berapa rata-rata nilai kamu? : ")
+if int(nilai) >= 90 :
+    print("Excellent performance")
+elif int(nilai) >= 80 :
+    print("Very Good performance")
+elif int(nilai) >= 70 :
+    print("good performance")
+elif int(nilai) >= 60 :
+    print("average performance")
+else:
+    print("Not my concern, sorry")
+
+
+#2
+print("A. 10, B. 99, C. 99,5")
+number="A","B"
+while number != "C":
+    number = input("Choose the big one! : ")
+print("Nice, good choice!")
+
